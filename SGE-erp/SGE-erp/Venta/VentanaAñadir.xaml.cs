cc4ff22f9b517ca8d16825c46854ba40994df79f
Noelia Perez <noepf_29@hotmail.com>
2019-11-11 17:00:06 +0100

    Añadir empleados

--- SGE-erp/SGE-erp/Venta/VentanaAñadir.xaml.cs
+++ SGE-erp/SGE-erp/Venta/VentanaAñadir.xaml.cs
@@ -24,5 +24,17 @@ namespace SGE_erp.Venta
         {
             InitializeComponent();
         }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            // No cargue datos en tiempo de diseño.
+            // if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+            // {
+            // 	//Cargue los datos aquí y asigne el resultado a CollectionViewSource.
+            // 	System.Windows.Data.CollectionViewSource myCollectionViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["Resource Key for CollectionViewSource"];
+            // 	myCollectionViewSource.Source = your data
+            // }
+        }
     }
 }
