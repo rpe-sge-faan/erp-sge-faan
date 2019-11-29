e3f22f384a0ab34b6d4e49c1c5acbdd32e5b9797
Andrea Lobo <andrea.lobo93@gmail.com>
2019-11-29 21:03:42 +0100

    Clientes completados

--- SGE-erp/SGE-erp/Gestion/Clientes.xaml.cs
+++ SGE-erp/SGE-erp/Gestion/Clientes.xaml.cs
@@ -25,54 +25,236 @@ namespace SGE_erp.Gestion
         public ClientesMain()
         {
             InitializeComponent();
-            
+            //Actualizar();
         }
 
-        ClientesEdicion c = null;
+        public static Delegate FiltrarLista;
+        public delegate void RefreshList();
+        public event RefreshList RefreshListEvent;
 
-        private void UserControl_Loaded_1(object sender, RoutedEventArgs e)
+        public delegate void FilterList();
+        public event FilterList FilterListEvent;
+
+        ClientesEdicion p = null;
+
+        private void Actualizar()
         {
-
-            // No cargue datos en tiempo de diseño.
-            // if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
-            // {
-            // 	//Cargue los datos aquí y asigne el resultado a CollectionViewSource.
-            // 	System.Windows.Data.CollectionViewSource myCollectionViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["Resource Key for CollectionViewSource"];
-            // 	myCollectionViewSource.Source = your data
-            // }
             try
             {
                 SqlConnection con = new SqlConnection(MetodosGestion.db);
                 DataSet ds = new DataSet();
-                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM [Proveedores]", con);
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM [Clientes]", con);
                 DataTable dt = new DataTable(); ;
 
-                ds.Clear();
+                // ds.Clear();
                 da.Fill(dt);
+
+                DataColumn tipoPro = new DataColumn("TipoP", typeof(string));
+                dt.Columns.Add(tipoPro);
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    if (dt.Rows[i]["Tipo"].Equals(1))
+                    {
+                        dt.Rows[i]["TipoP"] = "Particular";
+                    }
+                    else
+                    {
+                        dt.Rows[i]["TipoP"] = "Empresa";
+                    }
+                }
+
+                dt.Columns["TipoP"].SetOrdinal(3);
+
                 this.dataGridClientes.ItemsSource = dt.DefaultView;
 
                 con.Open();
                 con.Close();
+
+                dataGridClientes.Columns[0].Visibility = Visibility.Collapsed;
+                dataGridClientes.Columns[1].Visibility = Visibility.Collapsed;
             }
             catch
             {
                 return;
             }
-
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private void SetColumnsOrder(DataTable table, params String[] columnNames)
         {
-            
-            if (!MetodosGestion.IsOpen(c))
+            int columnIndex = 0;
+            foreach (var columnName in columnNames)
             {
-                c = new ClientesEdicion();
-                c.Owner = System.Windows.Application.Current.MainWindow;
-                c.Show();
-            }                  
-                    
+                table.Columns[columnName].SetOrdinal(columnIndex);
+                columnIndex++;
+            }
         }
 
+        private void UserControl_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            Actualizar();
+        }
 
+        private void Anadir_Click(object sender, RoutedEventArgs e)
+        {
+            if (!MetodosGestion.IsOpen(p))
+            {
+                p = new ClientesEdicion(0);
+                RefreshListEvent += new RefreshList(Actualizar);
+                p.Title = "Añadir Cliente";
+                p.Owner = System.Windows.Application.Current.MainWindow;
+                p.ActualizarLista = RefreshListEvent;
+                p.Show();
+            }
+        }
+
+        private void Actualizar_Click(object sender, RoutedEventArgs e)
+        {
+            Actualizar();
+        }
+
+        DataView view = null;
+        DataTable dt;
+        public void Filtrar()
+        {
+            List<String> nombres = AccesoVentana();
+            String[] campos = { "Nombre", "Telefono", "Email", "Persona_Contacto", "Direccion", "NIF", "TipoP" };
+
+            if (view == null)
+            {
+                view = new DataView();
+                dt = ((DataView)dataGridClientes.ItemsSource).ToTable();
+                dt.TableName = "Clientes";
+                view.Table = dt;
+            }
+
+            for (int i = 0; i < nombres.Count; i++)
+            {
+                view.RowFilter = $"{campos[i]} LIKE '%{nombres[i]}%'";
+            }
+
+            //view.Sort = "CompanyName DESC";
+            dt = view.ToTable();
+            dataGridClientes.ItemsSource = null;
+            dataGridClientes.ItemsSource = dt.DefaultView;
+            dataGridClientes.Columns[0].Visibility = Visibility.Collapsed;
+            dataGridClientes.Columns[1].Visibility = Visibility.Collapsed;
+
+        }
+        // params string[] nombres
+
+        public List<String> AccesoVentana()
+        {
+            List<String> nombres = new List<String>();
+            foreach (Window item in Application.Current.Windows)
+            {
+                //  0    1         2       3       4         5        6    7
+                // ID, NOMBRE, TELEFONO, EMAIL, CONTACTO, DIRECCION, NIF, TIPO
+                if (item.Name == "EdicionClientes")
+                {
+                    ((ClientesEdicion)item).personaContactoTextBox.IsEnabled = true;
+                    int tipo = ((ClientesEdicion)item).tipoComboBox.SelectedIndex;
+                    String t;
+                    if (tipo == 0)
+                    {
+                        t = "Particular";
+                    }
+                    else
+                    {
+                        t = "Empresa";
+                    }
+
+                    String[] nombresArray = {
+                        ((ClientesEdicion)item).nombreTextBox.Text,
+                        ((ClientesEdicion)item).telefonoTextBox.Text,
+                        ((ClientesEdicion)item).emailTextBox.Text,
+                        ((ClientesEdicion)item).personaContactoTextBox.Text,
+                        ((ClientesEdicion)item).direccionTextBox.Text,
+                        ((ClientesEdicion)item).nifTextBox.Text,
+                        t
+                    };
+                    nombres.AddRange(nombresArray);
+                }
+            }
+            return nombres;
+        }
+
+        private void Editar_Click(object sender, RoutedEventArgs e)
+        {
+            if (!MetodosGestion.IsOpen(p))
+            {
+                if (dataGridClientes.SelectedItem != null)
+                {
+                    DataRowView dd = (DataRowView)dataGridClientes.SelectedItem;
+                    int id = dd.Row.Field<int>("Id_Cliente");
+
+
+                    p = new ClientesEdicion(id);
+                    RefreshListEvent += new RefreshList(Actualizar);
+                    p.Title = "Editar Cliente";
+                    p.Owner = System.Windows.Application.Current.MainWindow;
+                    p.ActualizarLista = RefreshListEvent;
+                    p.Show();
+                }
+            }
+        }
+
+        private void Borrar_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGridClientes.SelectedItem != null)
+            {
+                DataRowView dd = (DataRowView)dataGridClientes.SelectedItem;
+                int id = dd.Row.Field<int>("Id_Cliente");
+                MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("¿Estás seguro?", "Confirmacion Borrado", System.Windows.MessageBoxButton.YesNo);
+                if (messageBoxResult == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        string bd = MetodosGestion.db;
+                        using (SqlConnection con = new SqlConnection(bd))
+                        using (SqlCommand command = con.CreateCommand())
+                        {
+                            command.CommandText = "DELETE FROM Clientes WHERE Id_Cliente = @id";
+
+                            command.Parameters.AddWithValue("@id", id);
+
+                            con.Open();
+                            int a = command.ExecuteNonQuery();
+
+                            if (a != 0)
+                            {
+                                con.Close();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Cliente ERROR al borrar");
+                            }
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    Actualizar();
+                }
+            }
+        }
+
+        private void Buscar_Click(object sender, RoutedEventArgs e)
+        {
+            if (!MetodosGestion.IsOpen(p))
+            {
+                FilterListEvent += new FilterList(Filtrar);
+                p = new ClientesEdicion(-1);
+                p.Title = "Buscar Cliente";
+                p.Owner = Application.Current.MainWindow;
+                p.FiltrarLista = FilterListEvent;
+                p.Show();
+            }
+            else
+            {
+                Filtrar();
+            }
+        }
     }
 }
--- SGE-erp/SGE-erp/Gestion/ClientesEdicion.xaml.cs
+++ SGE-erp/SGE-erp/Gestion/ClientesEdicion.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,20 +21,279 @@ namespace SGE_erp.Gestion
     /// </summary>
     public partial class ClientesEdicion : Window
     {
-        public ClientesEdicion()
+        public int id;
+        public Delegate ActualizarLista;
+        public Delegate FiltrarLista;
+        public delegate void RefreshList();
+        public event RefreshList RefreshListEvent;
+        public ClientesEdicion(int num)
         {
             InitializeComponent();
+
+            this.id = num;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            if (id == 0)
+            {
+                bAceptar.IsEnabled = false;
+            }
+            else if (id == -1)
+            {
+                tipoComboBox.SelectedIndex = -1;
+            }
+            else
+            {
+                string variable;
+                string bd = MetodosGestion.db;
+                using (SqlConnection con = new SqlConnection(bd))
+                using (SqlCommand command = con.CreateCommand())
+                {
+                    command.CommandText = "SELECT * FROM [Clientes] where Id_Cliente = @id";
+                    command.Parameters.AddWithValue("@id", id);
+                    con.Open();
 
-            SGE_erp.SetaData setaData = ((SGE_erp.SetaData)(this.FindResource("setaData")));
-            // Cargar datos en la tabla Clientes. Puede modificar este código según sea necesario.
-            SGE_erp.SetaDataTableAdapters.ClientesTableAdapter setaDataClientesTableAdapter = new SGE_erp.SetaDataTableAdapters.ClientesTableAdapter();
-            setaDataClientesTableAdapter.Fill(setaData.Clientes);
-            System.Windows.Data.CollectionViewSource clientesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("clientesViewSource")));
-            clientesViewSource.View.MoveCurrentToFirst();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        string[] columnas = new string[] { "Id_Cliente", "Nombre", "NIF", "Tipo", "Email", "Telefono", "Direccion", "PersonaContacto" };
+
+                        if (reader.Read())
+                        {
+                            variable = reader.GetString(reader.GetOrdinal("Nombre"));
+                            nombreTextBox.Text = variable;
+                            // id
+                            id_ClienteTextBox.Text = id.ToString();
+                            variable = reader.GetString(reader.GetOrdinal("NIF"));
+                            nifTextBox.Text = variable;
+
+                            int tipo = reader.GetInt32(reader.GetOrdinal("Tipo"));
+
+                            tipoComboBox.SelectedIndex = tipo - 1;
+
+                            variable = reader.GetString(reader.GetOrdinal("Email"));
+                            emailTextBox.Text = variable;
+                            variable = reader.GetString(reader.GetOrdinal("Telefono"));
+                            telefonoTextBox.Text = variable;
+                            variable = reader.GetString(reader.GetOrdinal("Direccion"));
+                            direccionTextBox.Text = variable;
+                            variable = reader.GetString(reader.GetOrdinal("PersonaContacto"));
+                            personaContactoTextBox.Text = variable;
+                        }
+                        // If you need to use all rows returned use a loop:
+                        //while (reader.Read())
+                        //{
+                        //    variable = reader.GetString(reader.GetOrdinal("Column"));
+                        //    MessageBox.Show(variable);
+                        //}
+                    }
+                }
+            }
+        }
+
+        private void Aceptar_Click(object sender, RoutedEventArgs e)
+        {
+            if (id == 0)
+            {
+                Nuevo();
+            }
+            else if (id == -1)
+            {
+                FiltrarLista.DynamicInvoke();
+            }
+            else
+            {
+                Editar();
+            }
+        }
+
+        private void Cancelar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Editar()
+        {
+            int tipo = tipoComboBox.SelectedIndex + 1;
+
+            try
+            {
+                string bd = MetodosGestion.db;
+                using (SqlConnection con = new SqlConnection(bd))
+                using (SqlCommand command = con.CreateCommand())
+                {
+                    command.CommandText = "UPDATE Clientes SET Tipo = @tipo, Nombre = @nombre, Telefono = @telefono, Email = @email, " +
+                        "PersonaContacto = @persona, Direccion = @direccion, NIF = @nif WHERE Id_Cliente = @id";
+
+                    command.Parameters.AddWithValue("@tipo", tipo);
+                    command.Parameters.AddWithValue("@nombre", nombreTextBox.Text);
+                    command.Parameters.AddWithValue("@telefono", telefonoTextBox.Text);
+                    command.Parameters.AddWithValue("@email", emailTextBox.Text);
+                    command.Parameters.AddWithValue("@persona", personaContactoTextBox.Text);
+                    command.Parameters.AddWithValue("@direccion", direccionTextBox.Text);
+                    command.Parameters.AddWithValue("@nif", nifTextBox.Text);
+                    command.Parameters.AddWithValue("@id", id);
+
+                    con.Open();
+                    int a = command.ExecuteNonQuery();
+
+
+
+                    //DataSet ds;
+                    //SqlDataAdapter da;
+                    //SqlCommandBuilder scb;
+                    //DataTable dt;
+
+                    //da = new SqlDataAdapter("SELECT * FROM [Clientes]", con);
+                    //ds = new DataSet();
+                    //dt = new DataTable();
+                    //ds.Clear();
+                    //da.Fill(dt);
+
+
+                    if (a != 0)
+                    {
+                        con.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cliente ERROR");
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            ActualizarLista.DynamicInvoke();
+
+            this.Close();
+        }
+
+        private void Nuevo()
+        {
+
+            int tipo;
+            if (tipoComboBox.SelectedValue.ToString() == "Particular") { tipo = 1; }
+            else { tipo = 2; }
+
+
+            try
+            {
+                string bd = MetodosGestion.db;
+                using (SqlConnection con = new SqlConnection(bd))
+                using (SqlCommand command = con.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO Clientes (Tipo, Nombre, Telefono, Email, PersonaContacto, Direccion, NIF) " +
+                        "VALUES (@tipo, @nombre, @telefono, @email, @persona, @direccion, @nif)";
+
+                    command.Parameters.AddWithValue("@tipo", tipo);
+                    command.Parameters.AddWithValue("@nombre", nombreTextBox.Text);
+                    command.Parameters.AddWithValue("@telefono", telefonoTextBox.Text);
+                    command.Parameters.AddWithValue("@email", emailTextBox.Text);
+                    command.Parameters.AddWithValue("@persona", personaContactoTextBox.Text);
+                    command.Parameters.AddWithValue("@direccion", direccionTextBox.Text);
+                    command.Parameters.AddWithValue("@nif", nifTextBox.Text);
+
+                    con.Open();
+                    int a = command.ExecuteNonQuery();
+
+                    if (a != 0)
+                    {
+                        con.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cliente ERROR");
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            ActualizarLista.DynamicInvoke();
+
+            this.Close();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void GenericTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (id != -1)
+            {
+                //(sender as TextBox).SelectAll();
+                //System.Diagnostics.Debug.WriteLine(txt.Name);
+                //emailTextBox_TextChanged(sender, e);
+                string nombre = ((sender as TextBox).Name).ToString();
+                switch (nombre)
+                {
+                    case "emailTextBox":
+                        try
+                        {
+                            var addr = new System.Net.Mail.MailAddress(emailTextBox.Text);
+                            if (addr.Address == emailTextBox.Text)
+                            {
+                                emailTextBox.ClearValue(TextBox.BackgroundProperty);
+                                labelInfo.Content = "";
+                            }
+                        }
+                        catch
+                        {
+                            emailTextBox.Background = (Brush)new BrushConverter().ConvertFrom("#FFBDAF");
+                            labelInfo.Content = "Error en el formato del email";
+                        }
+                        break;
+                    case "nifTextBox":
+                        Regex regex = new Regex("^[0-9]{8}[TtRrWwAaGgMmYyFfPpDdXxBbNnJjZzSsQqVvHhLlCcKkEe]$");
+                        if (regex.IsMatch(nifTextBox.Text))
+                        {
+                            nifTextBox.ClearValue(TextBox.BackgroundProperty);
+                            labelInfo.Content = "";
+                        }
+                        else
+                        {
+                            nifTextBox.Background = (Brush)new BrushConverter().ConvertFrom("#FFBDAF");
+                            labelInfo.Content = "Error en el formato del DNI";
+                        }
+                        break;
+                }
+                CheckAceptar();
+                //ClientesMain.AccesoVentana();
+            }
+        }
+
+        private void CheckAceptar()
+        {
+            bool enable = true;
+            var textBoxes = gridGeneral.Children.OfType<TextBox>();
+
+            foreach (TextBox txt in textBoxes)
+            {
+                if (txt.IsEnabled && txt.Name != "personaContactoTextBox")
+                {
+                    if (!txt.Background.ToString().Equals("#FFFFFFFF") || String.IsNullOrWhiteSpace(txt.Text))
+                    {
+                        enable = false;
+                    }
+                }
+            }
+
+            if (enable)
+            {
+                bAceptar.IsEnabled = true;
+            }
+            else
+            {
+                bAceptar.IsEnabled = false;
+            }
         }
     }
 }
