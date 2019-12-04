48aeffcdd4693dfb7f550e3b00994629f8370a7e
Aaron Perez Serrano <aaron.perez@juanxxiii.net>
2019-12-04 01:14:21 +0100

    Ya esta todo menos buscar

--- SGE-erp/SGE-erp/Articulos/EditaArticulos.xaml.cs
+++ SGE-erp/SGE-erp/Articulos/EditaArticulos.xaml.cs
@@ -15,122 +15,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-/*namespace SGE_erp.Articulos
-{
-    /// <summary>
-    /// Lógica de interacción para EditaArticulos.xaml
-    /// </summary>
-    public partial class EditaArticulos : Window
-    {
-        private int id;
-        public EditaArticulos(int num)
-        {
-            InitializeComponent();
-
-            this.id = num;
-        }
-
-        public Delegate ActualizarLista;
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-
-            if (id != 0)
-            {
-                string variable;
-                string bd = MetodosGestion.db;
-                using (SqlConnection con = new SqlConnection(bd))
-                using (SqlCommand command = con.CreateCommand())
-                {
-                    command.CommandText = "SELECT * FROM [Articulos] where Id_Articulo = @id";
-                    command.Parameters.AddWithValue("@id", id);
-                    con.Open();
-
-                    using (var reader = command.ExecuteReader())
-                    {
-                        string[] columnas = new string[] { "Id_Articulo", "Nombre", "Descripcion", "Tipo" };
-
-                        if (reader.Read())
-                        {
-                            variable = reader.GetString(reader.GetOrdinal("Nombre"));
-                            nombreTextBox1.Text = variable;
-                            // id
-                            id_ArticuloTextBox1.Text = id.ToString();
-                            variable = reader.GetString(reader.GetOrdinal("Descripcion"));
-                            descripcionTextBox1.Text = variable;
-
-                            int tipo = reader.GetInt32(reader.GetOrdinal("Id_Iva"));
-                            id_IvaComboBox1.SelectedIndex = tipo - 1;
-
-                            //int tipo2 = reader.GetInt32(reader.GetOrdinal("Tipo"));
-                            //tipoArticuloTextBox1.SelectedIndex = tipo2 - 1;
-
-                        }
-                        // If you need to use all rows returned use a loop:
-                        while (reader.Read())
-                        {
-                            variable = reader.GetString(reader.GetOrdinal("Column"));
-                            MessageBox.Show(variable);
-                        }
-                    }
-                }
-            }
-            else
-            {
-                bAceptar.IsEnabled = false;
-            }
-        }
-
-        private void Aceptar_Click(object sender, RoutedEventArgs e)
-        {
-            //int tipo;
-            //if (tipoComboBox.SelectedValue.ToString() == "Particular") { tipo = 1; }
-           // else { tipo = 2; }
-
-            try
-            {
-                string bd = MetodosGestion.db;
-                using (SqlConnection con = new SqlConnection(bd))
-                using (SqlCommand command = con.CreateCommand())
-                {
-                    command.CommandText = "INSERT INTO Proveedores (Id_Iva, Nombre, Descripcion, TipoArticulo) " +
-                        "VALUES (@Id_Iva, @nombre, @descripcion, @tipoArticulo)";
-
-                    command.Parameters.AddWithValue("@Id_Iva", id_IvaComboBox1.SelectedIndex+1);
-                    command.Parameters.AddWithValue("@nombre", id_ArticuloTextBox1.Text);
-                    command.Parameters.AddWithValue("@descripcion", descripcionTextBox1.Text);
-                    command.Parameters.AddWithValue("@tipoArticulo", tipoArticuloTextBox1.Text);
-
-                    con.Open();
-                    int a = command.ExecuteNonQuery();
-
-                    if (a != 0)
-                    {
-                        con.Close();
-                    }
-                    else
-                    {
-                        MessageBox.Show("ERROR articulos ");
-                    }
-                }
-            }
-            catch (SqlException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            ActualizarLista.DynamicInvoke();
-
-            this.Close();
-        }
-
-        private void Cancelar_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
-    }
-}*/
 
 namespace SGE_erp.Articulos
 {
@@ -191,7 +75,7 @@ namespace SGE_erp.Articulos
             }
             else
             {
-                bAceptar.IsEnabled = false;
+               // bAceptar.IsEnabled = false;
             }
         }
 
@@ -228,26 +112,12 @@ namespace SGE_erp.Articulos
                         "VALUES (@Id_Iva, @nombre, @descripcion, @tipoArticulo)";
 
                     command.Parameters.AddWithValue("@Id_Iva", id_IvaComboBox1.SelectedIndex + 1);
-                    command.Parameters.AddWithValue("@nombre", id_ArticuloTextBox1.Text);
+                    command.Parameters.AddWithValue("@nombre", nombreTextBox1.Text);
                     command.Parameters.AddWithValue("@descripcion", descripcionTextBox1.Text);
                     command.Parameters.AddWithValue("@tipoArticulo", tipo);
 
                     con.Open();
                     int a = command.ExecuteNonQuery();
-
-
-
-                    //DataSet ds;
-                    //SqlDataAdapter da;
-                    //SqlCommandBuilder scb;
-                    //DataTable dt;
-
-                    //da = new SqlDataAdapter("SELECT * FROM [Proveedores]", con);
-                    //ds = new DataSet();
-                    //dt = new DataTable();
-                    //ds.Clear();
-                    //da.Fill(dt);
-
 
                     if (a != 0)
                     {
@@ -272,11 +142,6 @@ namespace SGE_erp.Articulos
         private void nuevo()
         {
 
-            int tipo;
-            //if (tipoComboBox.SelectedValue.ToString() == "Particular") { tipo = 1; }
-            //else { tipo = 2; }
-
-
             try
             {
                 string bd = MetodosGestion.db;
@@ -287,9 +152,9 @@ namespace SGE_erp.Articulos
                         "VALUES (@Id_Iva, @nombre, @descripcion, @tipoArticulo)";
 
                     command.Parameters.AddWithValue("@Id_Iva", id_IvaComboBox1.SelectedIndex + 1);
-                    command.Parameters.AddWithValue("@nombre", id_ArticuloTextBox1.Text);
+                    command.Parameters.AddWithValue("@nombre", nombreTextBox1.Text);
                     command.Parameters.AddWithValue("@descripcion", descripcionTextBox1.Text);
-                    command.Parameters.AddWithValue("@tipoArticulo", tipoArticuloComboBox1.Text);
+                    command.Parameters.AddWithValue("@tipoArticulo", tipoArticuloComboBox1.SelectedIndex+1);
 
                     con.Open();
                     int a = command.ExecuteNonQuery();
