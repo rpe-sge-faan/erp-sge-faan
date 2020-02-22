9ffd8fcdf9fc3ad7138310b6d41974f9987f71db
Andrea Lobo <andrea.lobo93@gmail.com>
2020-02-23 00:21:00 +0100

    cuasi hecho

---
+++ SGE-erp/SGE-erp/Articulos/AddInventario.xaml.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SGE_erp.Gestion;
+
+namespace SGE_erp.Articulos
+{
+    public partial class AddInventario : Window
+    {
+        public Delegate ActualizarLista;
+
+        public AddInventario()
+        {
+            InitializeComponent();
+            RellenarTabla();
+        }
+
+        private void RellenarTabla()
+        {
+            SqlConnection con = new SqlConnection(MetodosGestion.db);
+            using (SqlDataAdapter da = new SqlDataAdapter("SELECT Id_Articulo, Nombre, Descripcion FROM Articulos", con))
+            {
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                DataColumn workCol = dt.Columns.Add("StockNuevo", typeof(Int32));
+                dataGridInventario.ItemsSource = dt.DefaultView;
+            }
+            con.Open();
+            con.Close();
+        }
+
+        private Boolean CheckRelleno()
+        {
+            DataTable dtaAnadir = ((DataView)dataGridInventario.ItemsSource).ToTable();
+            int cont = 0;
+            foreach (DataRow row in dtaAnadir.Rows)
+            {
+                if (row["StockNuevo"].GetType().ToString().Equals("System.DBNull"))
+                {
+                    //int indexAnadir = dataGridInventario.Items.IndexOf(row);
+                    //dtaAnadir.Rows[cont]["Stock"] = Convert.ToInt32(dtaAnadir.Rows[cont]["Stock"]) + stock;
+                    //break;
+                    return false;
+                }
+                cont++;
+            }
+            return true;
+        }
+
+        private void Grid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.Column.Header.ToString() == "Nombre")
+            {
+                e.Column.IsReadOnly = true; // Makes the column as read only
+            }
+            if (e.Column.Header.ToString() == "Id_Articulo")
+            {
+                e.Column.IsReadOnly = true; // Makes the column as read only
+            }
+            if (e.Column.Header.ToString() == "Descripcion")
+            {
+                e.Column.IsReadOnly = true; // Makes the column as read only
+            }
+        }
+
+        private void dataGridInventario_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            if (CheckRelleno())
+            {
+                guardarTabla.IsEnabled = true;
+            }
+            else
+            {
+                guardarTabla.IsEnabled = false;
+            }
+        }
+
+        private void dataGridInventario_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CheckRelleno())
+            {
+                guardarTabla.IsEnabled = true;
+            }
+            else
+            {
+                guardarTabla.IsEnabled = false;
+            }
+        }
+
+        private void dataGridInventario_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (CheckRelleno())
+            {
+                guardarTabla.IsEnabled = true;
+            }
+            else
+            {
+                guardarTabla.IsEnabled = false;
+            }
+        }
+
+        private void guardarTabla_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dtaAnadir = ((DataView)dataGridInventario.ItemsSource).ToTable();
+            string nombre = "";
+            int stock = 0;
+            decimal pvp = 0;
+            decimal precio = 0;
+
+            SqlConnection con = new SqlConnection(MetodosGestion.db);
+            using (SqlCommand command = con.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO Inventario (Fecha, IdEmpleado) OUTPUT INSERTED.Id VALUES (@fecha, @emple)";
+                command.Parameters.AddWithValue("@fecha", DateTime.Now);
+                command.Parameters.AddWithValue("@emple", MainWindow.idEmpleado);
+                con.Open();
+                int idInvent = command.ExecuteNonQuery();
+
+                foreach (DataRow row in dtaAnadir.Rows)
+                {
+                    using (SqlCommand anadir = con.CreateCommand())
+                    {
+                        anadir.CommandText = "INSERT INTO InventarioArticulos (IdInventario,IdArticulo,NombreArticulo,UnidadesContadas,UnidadesStock,PrecioVenta)" +
+                            " VALUES (@idi,@ida,@nombre,@contado,@stock,@venta)";
+
+                        int idArt = Convert.ToInt32(row["Id_Articulo"]);
+                        int comtado = Convert.ToInt32(row["StockNuevo"]);
+                        using (SqlCommand articulo = con.CreateCommand())
+                        {
+                            articulo.CommandText = "SELECT Nombre, Descripcion, PVP, Stock FROM Articulos WHERE Articulos.Id_Articulo = @id ";
+                            articulo.Parameters.AddWithValue("@id", idArt);
+                            using (SqlDataReader reader = articulo.ExecuteReader())
+                            {
+                                if (reader.Read())
+                                {
+                                    nombre = $"{reader[0].ToString()} {reader[1].ToString()}";
+                                    stock = Convert.ToInt32(reader[3]);
+                                    pvp = Convert.ToDecimal(reader[2]);
+                                    precio = Convert.ToDecimal(reader[4]);
+                                }
+                            }
+                        }
+
+                        anadir.Parameters.AddWithValue("@idi", idInvent);
+                        anadir.Parameters.AddWithValue("@ida", idArt);
+                        anadir.Parameters.AddWithValue("@nombre", nombre);
+                        anadir.Parameters.AddWithValue("@contado", idInvent);
+                        anadir.Parameters.AddWithValue("@stock", stock);
+                        //anadir.Parameters.AddWithValue("@compra", precio);
+                        anadir.Parameters.AddWithValue("@venta", pvp);
+
+                        anadir.ExecuteNonQuery();
+                        Console.WriteLine("GUARDADO");
+                    }
+                }
+                ActualizarLista.DynamicInvoke();
+            }
+            this.Close();
+        }
+    }
+}
